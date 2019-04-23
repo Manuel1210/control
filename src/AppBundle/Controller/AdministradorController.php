2c64b670c0a115421fb48c6511b8238514cfddbc
Manuel1210 <manuelescobar07@hotmail.com>
2019-04-22 22:26:35 -0600

    modificacion de reporte

--- src/AppBundle/Controller/AdministradorController.php
+++ src/AppBundle/Controller/AdministradorController.php
@@ -22,6 +22,13 @@ use AppBundle\Entity\Tipotransaccion;
 use AppBundle\Form\TipotransaccionType;
 use AppBundle\Form\InoutcajaType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
 
 class AdministradorController extends Controller
 {
@@ -428,9 +435,6 @@ public function asignarLiderAction($id){
     $ministerio = $this->getDoctrine()->getRepository(Ministerio::class)->findOneByIdministerio($idm);
     $em = $this->getDoctrine()->getManager();
 
-
-    
-
     $repositorylider = $this->getDoctrine()->getRepository(Liderministerio::class);
   
     $query = $repositorylider->createQueryBuilder('l')
@@ -551,7 +555,6 @@ public function asignarLiderAction($id){
       ->getQuery();
 
   $liderbusqueda = $query->getResult();
-
     $lider=null;
 
     if(count($liderbusqueda)>0){
@@ -569,7 +572,6 @@ public function asignarLiderAction($id){
       // mostrar imagenes
       //$path = $request->server->get('DOCUMENT_ROOT');    // C:/wamp64/www/
       //$path = rtrim($path, "/");                         // C:/wamp64/www
-
       $html = $this->renderView('pdfformat/ministerioPdf.html.twig',
       array('miembros'=>$miembros,'lider'=>$lider));
 
@@ -615,73 +617,30 @@ public function asignarLiderAction($id){
   }
 
   public function reporteMiembrosAction(){
-    return $this->render('administrador/reporteMiembros.html.twig');
+    $repository = $this->getDoctrine()->getRepository(Miembro::class);
+      $miembros = $repository->findAll();
+    return $this->render('administrador/reporteMiembros.html.twig',array(
+      'miembros' => $miembros
+    ));
   }
 
-  public function reporteMiembrosbautizadosAction(){
-
-    return $this->render('administrador/reporteMiembrosbautizados.html.twig');
-
-  }
-
-  public function reporteMinisteriosadministradorAction(){
-
-    return $this->render('administrador/reporteMinisteriosadministrador.html.twig');
-  }
-
-  public function reporteLideresadministradorAction(){
-    return $this->render('administrador/reporteLideresadministrador.html.twig');
-  }
-
-  public function reportePresentacionesAction(){ 
-    
-    return $this->render('administrador/reportePresentaciones.html.twig');
-
-  }
-
-  public function fechaReportetesoreriaadminAction(Request $request){
-    return $this->render('administrador/fechaReportetesoreriaadmin.html.twig');
-  }
-
-
-  //pdf de reportes
-
-  public function pdfMiembrosAction(){
+   //pdf de reportes de miembros
+  public function pdfMiembrosAction(Request $request){
     $miembros = $this->getDoctrine()->getRepository(Miembro::class)->findByEstado(1);
-    $repositorylider = $this->getDoctrine()->getRepository(Liderministerio::class);
+    //$repositorylider = $this->getDoctrine()->getRepository(Liderministerio::class);
   
-  $query = $repositorylider->createQueryBuilder('l')
-      ->where('l.ministerioministerio = :ministerio')
-      ->andWhere('l.estado = 1')
-      ->setParameter('ministerio', $ministerio)
-      ->getQuery();
-
-  $liderbusqueda = $query->getResult();
-
-    $lider=null;
-
-    if(count($liderbusqueda)>0){
-      $lider=$liderbusqueda[0];
-    }
-
     $snappy=$this->get("knp_snappy.pdf");
     $fecha_hoy=date("dmYHis");
-    
-    //$ubi="C:/wamp64/www/control/web/public/pdf/";
-    //$ubi="/home/Manuel1210/";
-      //$ubi="/var/www/html/control/web/public/pdf/";
-
-      $filename="rep_".$fecha_hoy.".pdf";
-      // mostrar imagenes
-      //$path = $request->server->get('DOCUMENT_ROOT');    // C:/wamp64/www/
-      //$path = rtrim($path, "/");                         // C:/wamp64/www
-
-      $html = $this->renderView('pdfformat/ministerioPdf.html.twig',
-      array('miembros'=>$miembros,'lider'=>$lider));
-
+    $ubi="C:\Users\Manuel\Documents";
+    $filename="rep_".$fecha_hoy.".pdf";
+    // mostrar imagenes
+    // $path = $request->server->get('DOCUMENT_ROOT');    // C:/wamp64/www/
+    //$path = rtrim($path, "/");                         // C:/wamp64/www
+    $html = $this->renderView('pdfformat/pdfMiembros.html.twig',
+      array('miembros'=>$miembros));
       //GENERAR PDF SIN RESPUESTAS
-      $response = new Response();
-      $response->setContent($this->get('knp_snappy.pdf')->generateFromHtml($html,$ubi.$filename,
+    $response = new Response();
+    $response->setContent($this->get('knp_snappy.pdf')->generateFromHtml($html,$ubi.$filename,
       array('lowquality' => false,
         'print-media-type' => true,
         'encoding' => 'utf-8',
@@ -693,12 +652,239 @@ public function asignarLiderAction($id){
         //'image-quality'=> '100',
         'header-font-size'=>7)));//guardamos el pdf
 
-      $response->setStatusCode(Response::HTTP_OK);
-      $response->headers->set('Content-Type', 'application/pdf');
+    $response->setStatusCode(Response::HTTP_OK);
+    $response->headers->set('Content-Type', 'application/pdf');
 
-      return $this->redirectToRoute('ministerio');
-      //return $response;
-      //return new Response("public/inventarioadmin/".$filename);
+    return new PdfResponse(
+      $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+        $filename
+    );
+  }
+
+   //
+  //funcion reporte  de presentacion 
+  public function reportePresentacionesAction(){
+    $repository= $this->getDoctrine()->getRepository(Presentacion::class);
+    $presentacion= $repository->findAll();
+    return $this->render('administrador/reportePresentaciones.html.twig',array(
+    'Presentacion'=> $presentacion
+    ));
+  }
+
+  //imprimir reporte presentacion 
+  public function pdfPresentacionesAction(Request $request){
+    $presentacion = $this->getDoctrine()->getRepository(Presentacion::class)->findAll();
+  
+    $snappy=$this->get("knp_snappy.pdf");
+    $fecha_hoy=date("dmYHis");
+  
+    $ubi="C:\Users\Manuel\Documents";
+    $filename="rep_".$fecha_hoy.".pdf";
+
+    $html = $this->renderView('pdfformat/pdfPresentaciones.html.twig',
+      array('Presentacion'=>$presentacion));
+      //GENERAR PDF SIN RESPUESTAS
+    $response = new Response();
+    $response->setContent($this->get('knp_snappy.pdf')->generateFromHtml($html,$ubi.$filename,
+      array('lowquality' => false,
+        'print-media-type' => true,
+        'encoding' => 'utf-8',
+        'page-size' => 'Letter',
+        'margin-top'=> '1cm',
+        'margin-bottom'=>'1cm',
+        'margin-left'=> '2cm',
+        'margin-right'=> '1cm',
+        //'image-quality'=> '100',
+        'header-font-size'=>7)));//guardamos el pdf
+    $response->setStatusCode(Response::HTTP_OK);
+    $response->headers->set('Content-Type', 'application/pdf');
+
+    return new PdfResponse(
+      $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+        $filename
+    );
+  }
+
+
+
+  public function reporteMiembrosbautizadosAction(Request $request){
+
+     $form=$this->createFormBuilder()
+      ->add('fechainicial',DateType::class, array(
+        // render as a single text box
+       // 'widget' => 'single_text',
+    // this is actually the default format for single_text
+       'format'      => 'yyyy-MM-dd', // you need to have 'y', 'M' and 'd' here
+       'years'       => range(date('Y'), date('Y') - 30, -1),
+        'attr'=>array('class'=>'form-control'),
+        ))
+      ->getForm();
+       $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $fecha=$form->get('fechainicial')->getData();
+        $anio=$fecha->format('Y');
+        $fechaini=new \DateTime($anio."-01-01 00:00:00");
+        $fechafin=new \DateTime($anio."-12-31 23:59:59");
+
+        $repository = $this->getDoctrine()->getRepository(Bautizo::class);
+
+        // createQueryBuilder() automatically selects FROM AppBundle:Product
+        // and aliases it to "p"
+        $query = $repository->createQueryBuilder('p')
+            ->where('p.fecha >= :fecha1')
+            ->andWhere('p.fecha <= :fecha2')
+            ->setParameter('fecha1', $fechaini)
+            ->setParameter('fecha2', $fechafin)
+            ->orderBy('p.fecha', 'ASC')
+            ->getQuery();
+        $bautizos = $query->getResult();
+
+        //return $this->render('administrador/mostrarReportebautizados.html.twig',array(
+        return $this->render('administrador/reporteMiembrosbautizadosresultbusqueda.html.twig',array(
+          'bautizos' => $bautizos
+        ));
+    }
+    //return $this->render('administrador/reporteMiembrosbautizados.html.twig',array(
+    return $this->render('administrador/reporteMiembrosbautizados.html.twig', array(
+      'form'=>$form->createView()
+    ));
+  }
+//////////////////////////////////////////////////////////////////////
+
+  public function reporteMinisteriosadministradorAction(){
+
+    return $this->render('administrador/reporteMinisteriosadministrador.html.twig');
+  }
+
+  public function reporteLideresadministradorAction(){
+    return $this->render('administrador/reporteLideresadministrador.html.twig');
+  }
+
+  
+  //public function reportePresentacionesAction(){ 
+    
+    //return $this->render('administrador/reportePresentaciones.html.twig');
+  //}
+
+
+  public function fechaReportetesoreriaadminAction(Request $request){
+    $form=$this->createFormBuilder()
+            ->add('fechainicial',DateType::class, array(
+                // render as a single text box
+                'widget' => 'single_text','label'=>'Fecha Inicio','attr'=>array('class'=>'form-control'),
+                ))
+            ->add('fechafinal',DateType::class, array(
+                // render as a single text box
+                'widget' => 'single_text','label'=>'Fecha Final','attr'=>array('class'=>'form-control'),
+                ))
+            ->add('tipo', ChoiceType::class, [
+                'choices'  => [
+                    'Ingresos' => 1,
+                    'Salidas' => 2,
+                ],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $fecha1=$form->get('fechainicial')->getData();
+            $fecha2=$form->get('fechafinal')->getData();
+            $tipo=$form->get('tipo')->getData();
+            //$t1 = $this->getDoctrine()->getRepository(Tipotransaccion::class)->findOneByTipo(2);
+
+            //SELECT * FROM `inoutcaja` INNER JOIN tipotransaccion ON tipotransaccion.idtipotransaccion = inoutcaja.tipotransaccion_idtipotransaccion WHERE tipotransaccion.tipo = 1;
+            if($fecha1<$fecha2){
+                $em = $this->getDoctrine()->getManager();
+
+                 $transaccionesquery = $em->createQuery(
+                  'SELECT i FROM  AppBundle:Inoutcaja i
+                  join AppBundle:Tipotransaccion t
+                  where t = i.tipotransacciontipotransaccion
+                  and t.tipo = :tipo
+                  and i.fecha >= :fechainicio
+                  and i.fecha <= :fechafinal')
+                  ->setParameter('fechainicio', $fecha1)
+                  ->setParameter('fechafinal', $fecha2)
+                  ->setParameter('tipo', $tipo);
+
+                  $transacciones= $transaccionesquery->getResult();
+
+
+                $caja = $this->getDoctrine()->getRepository(Caja::class)->findOneByIdcaja(1);
+                /*$repository=$this->getDoctrine()->getRepository(Inoutcaja::class);
+                $query = $repository->createQueryBuilder('f')
+                    ->where('r.tipo = 1')
+                    ->where('f.fecha >= :fechainicio')
+                    ->andWhere('f.fecha <= :fechafinal')
+                    ->andWhere('f.tipotransacciontipotransaccion.tipo = 1')
+                    ->setParameter('fechainicio', $fecha1)
+                    ->setParameter('fechafinal', $fecha2)
+                    //->setParameter('tipo',$t1)
+                    ->orderBy('f.fecha', 'ASC')
+                    ->getQuery();
+
+                $transacciones = $query->getResult();*/
+            }
+
+            return $this->render('administrador/resultadoReportetesoreriaadmin.html.twig',array(
+                'fecha1'=>$fecha1,
+                'fecha2'=>$fecha2,
+                'transacciones'=>$transacciones,
+                'caja'=>$caja
+            ));
+        }
+
+      return $this->render('administrador/fechaReportetesoreriaadmin.html.twig',
+      array('form'=>$form->createView()));
+
+    //return $this->render('administrador/fechaReportetesoreriaadmin.html.twig');
+  }
+
+  public function mostrarReportetesoreriaadminAction(Request $request){
+
+  }
+
+
+
+
+  public function pdfMiembrosbautizadosAction(request $request){
+    $bautizos = $this->getDoctrine()->getRepository(Bautizo::class);
+    //$repositorylider = $this->getDoctrine()->getRepository(Liderministerio::class);
+  
+    $snappy=$this->get("knp_snappy.pdf");
+    $fecha_hoy=date("dmYHis");
+  
+    $ubi="C:\Users\Manuel\Documents";
+
+    $filename="rep_".$fecha_hoy.".pdf";
+
+    $html = $this->renderView('pdfformat/pdfMiembrosbautizados.html.twig',
+      array('bautizos'=>$bautizo));
+
+      //GENERAR PDF SIN RESPUESTAS
+    $response = new Response();
+    $response->setContent($this->get('knp_snappy.pdf')->generateFromHtml($html,$ubi.$filename,
+      array('lowquality' => false,
+        'print-media-type' => true,
+        'encoding' => 'utf-8',
+        'page-size' => 'Letter',
+        'margin-top'=> '1cm',
+        'margin-bottom'=>'1cm',
+        'margin-left'=> '2cm',
+        'margin-right'=> '1cm',
+        //'image-quality'=> '100',
+        'header-font-size'=>7)));//guardamos el pdf
+
+    $response->setStatusCode(Response::HTTP_OK);
+    $response->headers->set('Content-Type', 'application/pdf');
+
+    return new PdfResponse(
+      $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+        $filename
+    );
+
   }
 
 }
